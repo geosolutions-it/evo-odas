cf0a9896312afb06a4f037d8b08abc0e5564d809
Fernando Ribeiro <fernandinand@gmail.com>
2016-10-05 10:44:46 +0200

    Merge landsat8 related classes and scripts.

---
+++ lsat8_process.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python
+
+import argparse
+import os
+import json
+from scripts import gdal
+
+
+def main():
+    parser = argparse.ArgumentParser(description="OWS12 Landsat script. Wrapper to landsat-util scripts.")
+    parser.add_argument('-b', '--bands', help='Select the bands to inject into GeoServer mosaic\n'
+                                              'Ex: 432',
+                        default='432')
+    parser.add_argument('-r', '--resample', nargs=1, default='nearest',
+                        choices=('nearest', 'average', 'gauss', 'cubic', 'cubicspline', 'lanczos', 'average_mp',
+                                 'average_magphase', 'mode'),
+                        help='Resample method to use on GDAL utils. default is nearest')
+    parser.add_argument('-c', '--config', nargs='?', help='Specific GDAL configuration string\n'
+                                                          'Ex: --config COMPRESS_OVERVIEW JPEG')
+    parser.add_argument('-o', '--overviews', nargs=1,
+                        help='Overviews to add to the target image')
+    parser.add_argument('-w', '--warp', nargs=1,
+                        help='The projection EPSG code to use for gdalwarp')
+    parser.add_argument('files', help='Mosaic files path')
+
+    args = parser.parse_args()
+
+    gd = gdal.GDAL()
+    # Add common options
+    gd.rmethod = args.resample[0]
+
+    if args.warp:
+        with open(os.path.join(args.files, 'ingest.txt'), 'r') as file:
+            for l in file:
+                row = json.loads(l)
+                for b in list(args.bands):
+                    if b in row[0][0]:
+                        granule = os.path.join(args.files, str(row[2]['properties']['sceneID']),
+                                     str(row[2]['properties']['sceneID']) + '_B' + b + '.TIF')
+                        output_granule = granule.replace('.TIF', '_WARPED.TIF')
+                        # TODO: Enable parallel processing
+                        gd.warp(granule, output_granule, args.warp[0])
+                        # Stick with original filenames
+                        os.remove(granule)
+                        if os.path.exists(output_granule[:-3] + 'IMD'):
+                            os.remove(output_granule[:-3] + 'IMD')
+                        os.rename(output_granule, granule)
+
+    if args.overviews:
+        scales = args.overviews[0].split(',')
+        with open(os.path.join(args.files, 'ingest.txt'), 'r') as file:
+            for l in file:
+                row = json.loads(l)
+                for b in list(args.bands):
+                    if b in row[0][0]:
+                        granule = os.path.join(args.files, str(row[2]['properties']['sceneID']),
+                                     str(row[2]['properties']['sceneID']) + '_B' + b + '.TIF')
+                        # TODO: Enable parallel processing
+                        gd.addOverviews(granule, scales, args.config)
+
+
+if __name__ == '__main__':
+    main()
---
+++ scripts/lsat8.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+
+import os
+from landsat.downloader import Downloader
+from landsat.search import Search
+from utils.utils import initLogger
+
+# Make logger global here
+logger = initLogger()
+
+
+class Landsat(object):
+    def __init__(self):
+        self.cloud_min = 0
+        self.geojson = True
+        self.download_list = 'download_files.txt'
+
+    def search(self, args):
+        try:
+            logger.debug('Executing landsat search with params %s' % args)
+
+            l_search = Search()
+
+            response = l_search.search(paths_rows=args.pathrow, lat=args.lat, lon=args.lon, start_date=args.start,
+                                       end_date=args.end, cloud_min=self.cloud_min, cloud_max=args.cloud,
+                                       limit=args.limit, geojson=self.geojson)
+
+            return response
+        except Exception as e:
+            logger.error('Exception querying landsat images with stacktrace %s' % (str(e)))
+            return False
+
+    def download(self, response_dict, download_dir):
+        try:
+            f = open(os.path.join(download_dir, self.download_list), 'w')
+            l_download = Downloader(download_dir=download_dir)
+            for s in response_dict:
+                scene = []
+                scene.append(str(s))
+                for b in response_dict[s]:
+                    if not b[1]:
+                        # Download per band, in case only 1 band missing...
+                        band = []
+                        band.append(b[0])
+                        l_download.download(scene, band)
+                        f.write(os.path.join(download_dir, str(s), str(s) + '_B' + str(b[0]) + '.TIF\n'))
+                        f.flush()
+            f.close()
+        except Exception as e:
+            logger.error('Exception downloading landsat images with stacktrace %s' % (str(e)))
+            return False
