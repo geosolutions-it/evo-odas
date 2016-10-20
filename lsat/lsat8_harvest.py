3b05ddf4a0e6546757569b7f487ccd8850dd3276
Fernando Ribeiro <fernandinand@gmail.com>
2016-10-20 12:30:40 +0200

    #15 - Delete granules older than given months parameter.

--- lsat/lsat8_harvest.py
+++ lsat/lsat8_harvest.py
@@ -2,7 +2,13 @@
 
 import argparse
 import os
+import sys
+import time
 from geoserver.catalog import Catalog
+from utils.utils import initLogger
+
+# Make logger global here
+logger = initLogger()
 
 
 def main():
@@ -11,7 +17,9 @@ def main():
     parser.add_argument('-c', '--catalog', default=('http://localhost:8080/geoserver/rest',
                                                     'admin', 'geoserver'),
                         nargs=3, help='Geoserver REST URL and authentication')
-    parser.add_argument('-s', '--store', nargs=1, help='The store name to retrieve coverages with workspace name (ows12:landsat)')
+    parser.add_argument('-s', '--store', nargs=1, help='The store name to retrieve coverages with workspace name '
+                                                       '(ows12:landsat)')
+    parser.add_argument('-d', '--delete', nargs=1, type=int, help='Delete granules older than given months value.')
     parser.add_argument('granules', help='Path to ingestion file.')
 
     args = parser.parse_args()
@@ -19,6 +27,9 @@ def main():
     catalog = Catalog(service_url=args.catalog[0], username=args.catalog[1], password=args.catalog[2])
     store = catalog.get_store(args.store[0].split(':')[1], args.store[0].split(':')[0])
     coverages = catalog.mosaic_coverages(store)
+    if not os.path.exists(os.path.join(args.granules, 'download_files.txt')):
+        logger.info('Missing file from previous download job.\nPlease run this job first!')
+        sys.exit(1)
     with open(os.path.join(args.granules, 'download_files.txt'), 'r') as file:
         for l in file:
             ingest = True
@@ -26,11 +37,24 @@ def main():
             for c in coverages['coverages']['coverage']:
                 granules = catalog.mosaic_granules(c['name'], store, filter='location like \'%' + gf + '%\'')
                 if len(granules['features']) > 0:
-                    print 'File ' + gf + ' already exists on database'
+                    logger.info('File ' + gf + ' already exists on database')
                     ingest = False
             if ingest:
+                logger.info('Ingesting granule ' + gf)
                 g = 'file://' + l.replace('\n', '')
                 catalog.harvest_externalgranule(g, store)
+
+    # Delete granules
+    if args.delete:
+        today = time.strftime('%Y-%m-%dT00:00:00')
+        for c in coverages['coverages']['coverage']:
+            granules = catalog.mosaic_granules(c['name'], store, filter='time before P' + str(args.delete[0]) + 'M/' + today)
+            if len(granules['features']) > 0:
+                for g in granules['features']:
+                    logger.info('Deleting granule ' + g['properties']['location'])
+                    catalog.mosaic_delete_granule(c['name'], store, g['id'])
+            else:
+                logger.info('No granules to delete!')
 
 if __name__ == '__main__':
     main()
