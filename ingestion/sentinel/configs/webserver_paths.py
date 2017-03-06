627b7cf8eeb8d89ce6c57697a263faa9f7d1574e
Damianofds <damianofds@gmail.com>
2017-03-06 10:54:23 -0800

    added product htmlDescription support persisting thumbs on fs + draft of test system

---
+++ ingestion/sentinel/configs/webserver_paths.py
@@ -0,0 +1,4 @@
+#The Webserver document root, absolute path ending with a slash
+document_root = '/tmp/'
+# the name of the preview folder under the document root
+preview_folder = 'evo_odas_previews/'
---
+++ ingestion/sentinel/test_sentinel2_metadata_ingestion.py
@@ -0,0 +1,22 @@
+import sys
+import s2reader
+import utils.metadata_utils as mu
+import ssat2_metadata as s2
+
+def test_metadata_read(pkg_path):
+    with s2reader.open(pkg_path) as safe_pkg:
+        mu.print_metadata(safe_pkg)
+
+def test_product_abstract_generation(pkg_path):
+    with s2reader.open(pkg_path) as safe_pkg:
+        for granule in safe_pkg.granules:
+            (search_params, other_metadata, product_abstract_metadata) = s2.collect_sentinel2_metadata(safe_pkg, granule)
+            print s2.generate_product_abstract(product_abstract_metadata)
+
+def main(args):
+    pkg_path = "test_data/S2A_OPER_PRD_MSIL1C_PDMC_20160929T185902_R065_V20160929T102022_20160929T102344.SAFE"
+    test_product_abstract_generation(pkg_path)
+    test_metadata_read(pkg_path)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
