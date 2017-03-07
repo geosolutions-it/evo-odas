84f61eacfd479818183fad6b6e32a554d4dbc7c1
Damianofds <damianofds@gmail.com>
2017-03-06 16:06:58 -0800

    refactoring, externalization of templates resolver and persistances behaviours

---
+++ ingestion/sentinel/utils/metadata_storage.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+import pg_simple
+import configs.metadata_db_connection_params as db_config
+
+class PostgresStorage:
+    """Implementation of the EVO-ODAS Search Engine storage using the Postgres relational DBMS with PostGIS spatial extension"""
+
+    def __init__(self):
+        pg_simple.config_pool(max_conn=db_config.max_conn,
+                                expiration=db_config.expiration,
+                                host=db_config.host,
+                                port=db_config.port,
+                                database=db_config.database,
+                                user=db_config.user,
+                                password=db_config.password)
+
+    def check_granule_identifier(self, granule_identifier):
+
+        with pg_simple.PgSimple() as db:
+            collection = db.fetchone('metadata.product',
+                                        fields=['"name"'],
+                                        where=('"name" = %s', [granule_identifier]))
+            if collection == None:
+                return False
+            else:
+                return True
+
+    def persist_product_search_params(self, dict_to_persist):
+        with pg_simple.PgSimple() as db:
+            collection = db.fetchone('metadata.collection',
+                                        fields=['"eoIdentifier"'],
+                                        where=('"eoIdentifier" = %s', ["SENTINEL2"]))
+            if collection is None:
+                raise LookupError("ERROR: No related collection found!")
+            dict_to_persist['"eoParentIdentifier"'] = collection.eoIdentifier
+            row = db.insert('metadata.product', data=dict_to_persist, returning='id')
+            db.commit()
+        return row.id
+
+    def persist_product_metadata(self, xmlDoc, id):
+        with pg_simple.PgSimple() as db:
+            db.insert('metadata.product_metadata', data={'metadata':xmlDoc, 'id':id})
+            db.commit()
