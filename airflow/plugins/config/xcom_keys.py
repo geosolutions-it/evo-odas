1c22c7a27990ca5880fc25c892390696a851ad82
moataz <maabdelghaffar@gmail.com>
2017-08-25 16:45:17 +0800

    adding back config folder and evo-odas plugin

---
+++ airflow/plugins/config/xcom_keys.py
@@ -0,0 +1,4 @@
+PACKAGE_LOCATION_XCOM_KEY = 'package_location'
+IMAGE_ZIP_ABS_PATH_PREFIX_XCOM_KEY = 'img_zip_abs_path_'
+WORKDIR_IMG_NAME_PREFIX_XCOM_KEY = 'workdir_img_name_'
+GRANULE_TO_UPLOAD_PREFIX = 'granule_prefix_'
