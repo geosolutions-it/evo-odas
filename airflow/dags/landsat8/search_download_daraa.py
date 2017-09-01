91762933b6d4b0d49aa74bd1b6ba87ea49f59425
Ricardo Garcia Silva <ricardo.garcia.silva@gmail.com>
2017-09-02 00:44:00 +0100

    simplified Landsat8 DAG files
    Removed redundant dicts and brought parameters closer
    to where they are used

--- airflow/dags/landsat8/search_download_daraa.py
+++ airflow/dags/landsat8/search_download_daraa.py
@@ -16,75 +16,22 @@ from airflow.operators import Landsat8ThumbnailOperator
 
 from landsat8.secrets import postgresql_credentials
 
-# General and shared configuration between tasks
-daraa_default_args = {
-    'start_date': datetime(2017, 1, 1),
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'provide_context': True,
-    'email': ['xyz@xyz.com'],
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
-    'max_threads': 1,
-}
-
-# Task specific configuration
-daraa_search_args = {
-    # 'acquisition_date': '2017-04-11 05:36:29.349932',
-    'cloud_coverage': 90.9,
-    'path': 174,
-    'row': 37,
-    'pgdbname': postgresql_credentials['dbname'],
-    'pghostname': postgresql_credentials['hostname'],
-    'pgport': postgresql_credentials['port'],
-    'pgusername': postgresql_credentials['username'],
-    'pgpassword': postgresql_credentials['password'],
-}
-
-daraa_download_args = {
-    'download_dir': '/var/data/download/',
-    'number_of_bands': 1
-}
-
-daraa_translate_args = {
-    'working_dir': '/var/data/download/',
-    'blockx_size': '512',
-    'blocky_size': '512',
-    'tiled': 'YES',
-    'b': '1',
-    'ot': 'UInt16',
-    'of': 'GTiff',
-    'outsize': '',
-    'scale': ''
-}
-
-daraa_addo_args = {
-    'resampling_method': 'average',
-    'max_overview_level': 128,
-    'compress_overview': 'PACKBITS',
-    'photometric_overview': 'MINISBLACK',
-    'interleave_overview': ''
-}
-
-product_thumbnail_args = {
-    'thumb_size_x': '64',
-    'thumb_size_y': '64'
-}
-
-metadata_args = {
-    'metadata_xml_path': './geo-solutions-work/evo-odas/metadata-ingestion/'
-                         'templates/metadata.xml',
-    'description_template': './geo-solutions-work/evo-odas/metadata-ingestion/'
-                            'templates/product_abstract.html',
-    'loc_base_dir': '/efs/geoserver_data/coverages/landsat8/daraa'
-}
 
 daraa_dag = DAG(
     'Search_daraa_Landsat8',
     description='DAG for searching Daraa AOI in Landsat8 data from '
                 'scenes_list',
-    default_args=daraa_default_args,
+    default_args={
+        'start_date': datetime(2017, 1, 1),
+        'owner': 'airflow',
+        'depends_on_past': False,
+        'provide_context': True,
+        'email': ['xyz@xyz.com'],
+        'email_on_failure': False,
+        'email_on_retry': False,
+        'retries': 1,
+        'max_threads': 1,
+    },
     dagrun_timeout=timedelta(hours=1),
     schedule_interval=timedelta(days=1),
     catchup=False
@@ -92,33 +39,33 @@ daraa_dag = DAG(
 
 search_daraa_task = Landsat8SearchOperator(
     task_id='landsat8_search_daraa_task',
-    cloud_coverage=daraa_search_args['cloud_coverage'],
-    path=daraa_search_args['path'],
-    row=daraa_search_args['row'],
-    pgdbname=daraa_search_args['pgdbname'],
-    pghostname=daraa_search_args['pghostname'],
-    pgport=daraa_search_args['pgport'],
-    pgusername=daraa_search_args['pgusername'],
-    pgpassword=daraa_search_args['pgpassword'],
+    cloud_coverage=90.9,
+    path=174,
+    row=37,
+    pgdbname=postgresql_credentials['dbname'],
+    pghostname=postgresql_credentials['hostname'],
+    pgport=postgresql_credentials['port'],
+    pgusername=postgresql_credentials['username'],
+    pgpassword=postgresql_credentials['password'],
     dag=daraa_dag
 )
 
 download_daraa_task = Landsat8DownloadOperator(
     task_id='landsat8_download_daraa_task',
-    download_dir=daraa_download_args['download_dir'],
-    number_of_bands=daraa_download_args['number_of_bands'],
+    download_dir="/var/data/download",
+    number_of_bands=1,
     dag=daraa_dag
 )
 
 translate_daraa_task = GDALTranslateOperator(
     task_id='landsat8_translate_daraa_task',
-    working_dir=daraa_translate_args["working_dir"],
-    blockx_size=daraa_translate_args['blockx_size'],
-    blocky_size=daraa_translate_args['blocky_size'],
-    tiled=daraa_translate_args['tiled'],
-    b=daraa_translate_args['b'],
-    ot=daraa_translate_args['ot'],
-    of=daraa_translate_args['of'],
+    working_dir="/var/data/download",
+    blockx_size="512",
+    blocky_size="512",
+    tiled="YES",
+    b="1",
+    ot="UInt16",
+    of="GTiff",
     dag=daraa_dag
 )
 
@@ -127,30 +74,32 @@ addo_daraa_task = GDALAddoOperator(
     xk_pull_dag_id='Search_daraa_Landsat8',
     xk_pull_task_id='landsat8_translate_daraa_task',
     xk_pull_key_srcfile='translated_scenes_dir',
-    resampling_method=daraa_addo_args['resampling_method'],
-    max_overview_level=daraa_addo_args['max_overview_level'],
-    compress_overview=daraa_addo_args['compress_overview'],
-    photometric_overview=daraa_addo_args['photometric_overview'],
-    interleave_overview=daraa_addo_args['interleave_overview'],
+    resampling_method="average",
+    max_overview_level=128,
+    compress_overview="PACKBITS",
+    photometric_overview="MINISBLACK",
+    interleave_overview="",
     dag=daraa_dag
 )
 
 product_json_task = Landsat8MTLReaderOperator(
     task_id='landsat8_product_json_task',
-    loc_base_dir=metadata_args["loc_base_dir"],
-    metadata_xml_path=metadata_args["metadata_xml_path"],
+    loc_base_dir='/efs/geoserver_data/coverages/landsat8/daraa',
+    metadata_xml_path='./geo-solutions-work/evo-odas/metadata-ingestion/'
+                      'templates/metadata.xml',
     dag=daraa_dag
 )
 
 product_thumbnail_task = Landsat8ThumbnailOperator(
     task_id='landsat8_product_thumbnail_task',
-    thumb_size_x=product_thumbnail_args['thumb_size_x'],
-    thumb_size_y=product_thumbnail_args['thumb_size_y'],
+    thumb_size_x="64",
+    thumb_size_y="64",
     dag=daraa_dag
 )
 
 product_description_task = Landsat8ProductDescriptionOperator(
-    description_template=metadata_args["description_template"],
+    description_template='./geo-solutions-work/evo-odas/metadata-ingestion/'
+                         'templates/product_abstract.html',
     task_id='landsat8_product_description_task',
     dag=daraa_dag
 )
