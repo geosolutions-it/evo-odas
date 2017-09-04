54731a82046f9c9374420efea3448e60ce3091b5
Ricardo Garcia Silva <ricardo.garcia.silva@gmail.com>
2017-09-04 03:31:35 +0100

    refactored the update_scene_list dag in order to be able to test it
    the changes in this commit refactor both the dag and its custom operators.
    they were necessary in order to be able to test tasks in isolation.
    these changes drop xcoms in this dag.

--- airflow/dags/landsat8/landsat_update_scene_list.py
+++ airflow/dags/landsat8/landsat_update_scene_list.py
@@ -1,13 +1,16 @@
 from datetime import datetime, timedelta
+from getpass import getuser
+import os
 
 from airflow import DAG
-from airflow.operators import BaseOperator
-from airflow.operators import BashOperator
 from airflow.operators import DownloadSceneList
 from airflow.operators import ExtractSceneList
 from airflow.operators import UpdateSceneList
 
 from landsat8.secrets import postgresql_credentials
+
+DOWNLOAD_URL = 'http://landsat-pds.s3.amazonaws.com/c1/L8/scene_list.gz'
+DOWNLOAD_DIR = os.path.join(os.path.expanduser("~"), "download")
 
 landsat8_scene_list = DAG(
     'Landsat8_Scene_List',
@@ -15,7 +18,7 @@ landsat8_scene_list = DAG(
                 'into postgres db',
     default_args={
         'start_date': datetime(2017, 1, 1),
-        'owner': 'airflow',
+        'owner': getuser(),
         'depends_on_past': False,
         'provide_context': True,
         'email': ['xyz@xyz.com'],
@@ -23,27 +26,37 @@ landsat8_scene_list = DAG(
         'email_on_retry': False,
         'retries': 1,
         'max_threads': 1,
-        'download_dir': '/var/data/download/'
     },
     dagrun_timeout=timedelta(hours=1),
     schedule_interval=timedelta(days=1),
     catchup=False
 )
 
+# more info on Landsat products on AWS at:
+# https://aws.amazon.com/public-datasets/landsat/
 download_scene_list_gz = DownloadSceneList(
     task_id='download_scene_list_gz_task',
-    download_url='http://landsat-pds.s3.amazonaws.com/c1/L8/scene_list.gz',
+    download_url=DOWNLOAD_URL,
+    download_dir=DOWNLOAD_DIR,
     dag=landsat8_scene_list
 )
 
 extract_scene_list = ExtractSceneList(
     task_id='extract_scene_list_task',
+    path_to_extract=os.path.join(
+        DOWNLOAD_DIR,
+        DOWNLOAD_URL.rpartition("/")[-1]
+    ),
     dag=landsat8_scene_list
 )
 
 update_scene_list_db = UpdateSceneList(
     task_id='update_scene_list_task',
-    pg_dbname=postgresql_credentials['pg_dbname'],
+    scene_list_path=os.path.join(
+        DOWNLOAD_DIR,
+        "{}.csv".format(os.path.splitext(DOWNLOAD_URL.rpartition("/")[-1]))
+    ),
+    pg_dbname=postgresql_credentials['dbname'],
     pg_hostname=postgresql_credentials['hostname'],
     pg_port=postgresql_credentials['port'],
     pg_username=postgresql_credentials['username'],
--- airflow/plugins/landsat8_db_plugin.py
+++ airflow/plugins/landsat8_db_plugin.py
@@ -1,70 +1,75 @@
-from datetime import datetime, timedelta
+from datetime import timedelta
 import gzip
 import logging
+import os
 import pprint
-import urllib
 
-import psycopg2
-from airflow.operators import BaseOperator, BashOperator
+from airflow.operators import BaseOperator
 from airflow.operators import BashOperator
 from airflow.plugins_manager import AirflowPlugin
 from airflow.utils.decorators import apply_defaults
+import psycopg2
+import requests
 
-log = logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
 pp = pprint.PrettyPrinter(indent=2)
+
+
+def download_file(url, destination_directory):
+    response = requests.get(url, stream=True)
+    try:
+        os.makedirs(destination_directory)
+    except OSError as exc:
+        if exc.errno == 17:
+            pass  # directory already exists
+        else:
+            raise
+    full_path = os.path.join(destination_directory, url.rpartition("/")[-1])
+    with open(full_path, "wb") as fh:
+        for chunk in response.iter_content(chunk_size=1024):
+            fh.write(chunk)
+    return full_path
 
 
 class DownloadSceneList(BaseOperator):
 
     @apply_defaults
-    def __init__(self, download_url, download_dir,
-                 download_timeout=timedelta(hours=1), *args, **kwargs):
+    def __init__(self, download_url, download_dir, *args, **kwargs):
+        super(DownloadSceneList, self).__init__(*args, **kwargs)
         self.download_url = download_url
         self.download_dir = download_dir
-        log.info('-------------------- DownloadSceneList ------------')
-        super(DownloadSceneList, self).__init__(*args, **kwargs)
 
     def execute(self, context):
-        log.info('-------------------- DownloadSceneList Execute------------')
-        urllib.urlretrieve(self.download_url,
-                           self.download_dir+"scene_list.gz")
-        context['task_instance'].xcom_push(
-            key='scene_list_gz_path',
-            value=str(self.download_dir)+"scene_list.gz"
-        )
+        logger.info("Downloading {!r}...".format(self.download_url))
+        download_file(self.download_url, self.download_dir)
         return True
 
 
 class ExtractSceneList(BaseOperator):
 
     @apply_defaults
-    def __init__(self, download_dir, *args, **kwargs):
-        log.info('-------------------- ExtractSceneList ------------')
-        self.download_dir = download_dir
+    def __init__(self, path_to_extract, *args, **kwargs):
         super(ExtractSceneList, self).__init__(*args, **kwargs)
+        self.path_to_extract = path_to_extract
 
     def execute(self, context):
-        log.info('-------------------- ExtractSceneList Execute------------')
-        scene_list_gz_path = context['task_instance'].xcom_pull(
-            'download_scene_list_gz_task', key='scene_list_gz_path')
-        with gzip.open(scene_list_gz_path, 'rb') as f:
-             file_content = f.read()
-        outF = file(self.download_dir+"scene_list.csv", 'wb')
-        outF.write(file_content)
-        outF.close()
-        context['task_instance'].xcom_push(
-            key='scene_list_csv_path',
-            value=str(self.download_dir)+"scene_list.csv"
-        )
+        target_path = "{}.csv".format(
+            os.path.splitext(self.path_to_extract)[0])
+        logger.info("Extracting {!r} to {!r}...".format(
+            self.path_to_extract, target_path))
+        with gzip.open(self.path_to_extract, 'rb') as zipped_fh, \
+                open(target_path, "wb") as extracted_fh:
+            extracted_fh.write(zipped_fh.read())
         return True
 
 
 class UpdateSceneList(BaseOperator):
 
     @apply_defaults
-    def __init__(self, pg_dbname, pg_hostname, pg_port, pg_username,
+    def __init__(self, scene_list_path, pg_dbname, pg_hostname, pg_port,
+                 pg_username,
                  pg_password,*args, **kwargs):
-        log.info('-------------------- UpdateSceneList ------------')
+        self.scene_list_path = scene_list_path
         self.pg_dbname = pg_dbname
         self.pg_hostname = pg_hostname
         self.pg_port = pg_port
@@ -73,32 +78,24 @@ class UpdateSceneList(BaseOperator):
         super(UpdateSceneList, self).__init__(*args, **kwargs)
 
     def execute(self, context):
-        log.info('-------------------- UpdateSceneList Execute------------')
-        scene_list_csv_path = context['task_instance'].xcom_pull(
-            'extract_scene_list_task', key='scene_list_csv_path')
-        delete_first_line_cmd = (
-            "tail -n +2 " + scene_list_csv_path + "> " +
-            scene_list_csv_path + ".tmp && mv "+ scene_list_csv_path +
-            ".tmp  " + scene_list_csv_path
-        )
-        delete_line_operator = BashOperator(
-            task_id='Delete_first_line_OP',
-            bash_command = delete_first_line_cmd
-        )
-        delete_line_operator.execute(context)
-        db = psycopg2.connect(
+        logger.info("Connecting to '{}/{}'...".format(
+            self.pg_hostname, self.pg_dbname))
+        db_connection = psycopg2.connect(
             "dbname='{}' user='{}' host='{}' password='{}'".format(
                 self.pg_dbname, self.pg_username, self.pg_hostname,
                 self.pg_password
             )
         )
-        cursor = db.cursor()
-        cursor.execute("delete from scene_list")
-        db.commit()
-        fo = open(scene_list_csv_path, 'r')
-        cursor.copy_from(fo, 'scene_list', sep=',')
-        db.commit()
-        fo.close()
+        logger.info("Deleting previous data from db...")
+        with db_connection as conn:
+            with conn.cursor() as cursor:
+                cursor.execute("DELETE FROM scene_list;")
+        logger.info("Loading data from {!r} into db...".format(
+            self.scene_list_path))
+        with db_connection as conn, open(self.scene_list_path) as fh:
+            fh.readline()
+            with conn.cursor() as cursor:
+                cursor.copy_from(fh, "scene_list", sep=",")
         return True
 
 
