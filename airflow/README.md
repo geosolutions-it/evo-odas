afdc5c2f3be7b1f0d8bcde5bf1bcf66a77ce2920
torse <torse@mailbox.org>
2017-09-12 17:24:31 +0200

    Refactor S2 DAG and plugins to use new configuration management
    This commit replaces the existing `Sentinel2.py` DAG with a new `S2_MSI_L1C.py` DAG that follows the naming conventions and concepts detailed in #118. The new config module now provides core settings and collection specific settings in python modules. These configuration items are represented in a flat hierarchy to facilitate overriding single items as well as make them easily used in the application code.
    
    Furthermore the following improvements have been made to the plugins and docker files.
    
    - Operators have been updated to a) `return` outputs within it's `execute` methods and b) supports getting the inputs from XCOM keys defined in `get_inputs_from` parameter (as suggested by @ricardogsilva in #132). However the operators still support passing the legacy parameters for the XCOM (like `xk_pull_dag_id`).
    
    - The configuration items have been harmonised, e.g. `coverage_id` and `GS_WMS_WIDTH` have been renamed to `geoserver_coverage` and `geoserver_oseo_wms_width` in the configuration (the operators are TBD).
    
    - The Dockerfile and `myairflow` script has been updated to use Airflow 1.8.1 and GDAL 2.2.1. Furthermore some mounted paths have been fixed and the `airflow_test` command now tests the complete `S2_MSI_L1C` tasks. This is helpful for local development and debugging:
    ```
    $ source myairflow
    
    $ airflow_init
    
    $ airflow_build
    
    $ airflow_start
    
    $ airflow_test
    
    $ ./myairflow help
    USAGE: ./myairflow [status|exec|init|test|update|build|start|stop|restart|remove|login|logs]
    
    ```
    
    Resolves: #118
    See also: #132

--- airflow/README.md
+++ airflow/README.md
@@ -1,4 +1,17 @@
-* The `dags` and `operators` folders are structured to be directly imported in the [Airflow Home directory](https://airflow.incubator.apache.org/start.html#quick-start).
+# Airflow
+
+## Directory Structure
+
+* The `dags` and `plugins` folders are structured to be directly imported in the [Airflow Home directory](https://airflow.incubator.apache.org/start.html#quick-start).
 They can be copied or linked inside the home directory.
 
-* The `docker` folder contain a docker file to build up a docker image with an airflow test environment
+* The `docker` folder contain a docker file to build up a docker image with an airflow test environment. To test
+
+* The `config` folder contain configuration items as python scripts that can be overriden by environment specific files in `config/override`.
+
+
+## Testing
+
+For development and testing it is helpful to mount folders into the docker container
+
+
---
+++ airflow/dags/config/s1_grd_1sdh.py
@@ -0,0 +1,37 @@
+from datetime import datetime, timedelta
+import os
+import config
+
+#
+# Collection
+#
+id = "S1_GRD_1SDH"
+filename_filter = "S1?_EW_GRDM_1SDH*"
+platformname = 'Sentinel-1'
+
+collection_dir = os.path.join(config.base_dir, platformname, id)
+download_dir = os.path.join(collection_dir,"download")
+process_dir = os.path.join(collection_dir,"process")
+upload_dir = os.path.join(collection_dir,"upload")
+repository_dir = os.path.join(config.repository_base_dir, "SENTINEL/S1/", id, "v0")
+
+#
+# DHUS specific
+#
+dhus_filter_max = 2
+dhus_search_bbox = os.path.join(config.regions_base_dir,'europe.geojson')
+dhus_search_filename = filename_filter
+dhus_search_startdate = datetime.today() - timedelta(days=4)
+dhus_search_startdate = dhus_search_startdate.isoformat() + 'Z'
+dhus_search_enddate = datetime.now().isoformat() + 'Z'
+dhus_search_orderby = '-ingestiondate'
+dhus_search_keywords = {
+        'filename': filename_filter,
+        'platformname': platformname,
+        'orbitdirection':'Descending',
+}
+
+try:
+    from override.s2_msi_l1c import *
+except:
+    pass
