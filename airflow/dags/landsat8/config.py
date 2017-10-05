340ca5f8051148a8cd0571e25c2b8fe1777ad6c5
Alessandro Parma <alessa.parma@gmail.com>
2017-10-05 12:53:24 +0200

    Implement granules upload for L8 #220
    Add XCom push to GDALAddo with path to processed files. Granules upload task use this to determine which files to upload
    Add configu file for L8 to hold rsync operator config. This will be replaced soon in favor of usual conf. management
    Renamed DAG files and DAG names to more appropriate names

---
+++ airflow/dags/landsat8/config.py
@@ -0,0 +1,4 @@
+rsync_hostname="cloudsdi.geo-solutions.it"
+rsync_username="ec2-user"
+rsync_ssh_key_file="/usr/local/airflow/id_rsa"
+rsync_remote_dir="/efs/geoserver_data/airflow_data/repository/LANDSAT/L8"
