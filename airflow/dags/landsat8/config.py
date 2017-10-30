dd567c18e87f3a30ba6fe6f5d781250b9aafe6ac
Alessandro Parma <alessa.parma@gmail.com>
2017-10-30 13:42:06 +0100

    remove unused files from repo

--- airflow/dags/landsat8/config.py
+++
@@ -1,7 +0,0 @@
-rsync_hostname="cloudsdi.geo-solutions.it"
-rsync_username="ec2-user"
-rsync_ssh_key_file="/usr/local/airflow/id_rsa"
-rsync_remote_dir="/efs/geoserver_data/airflow_data/repository/LANDSAT/L8"
-
-geoserver_rest_url="http://cloudsdi.geo-solutions.it/geoserver/rest"
-geoserver_oseo_collection="LANDSAT8"
