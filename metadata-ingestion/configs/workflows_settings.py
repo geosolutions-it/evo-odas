90a839674c4825099be668bbb60fb2c31cb66f99
Alessandro Parma <alessa.parma@gmail.com>
2017-10-30 13:46:50 +0100

    remove unused files from repo

--- metadata-ingestion/configs/workflows_settings.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-# add the trailing slash
-working_directory = "/tmp/"
-thumb_size_x = "64"
-thumb_size_y = "64"
-thumb_jpeg_compression_quality = 80
-original_package_location_path = "${BASE_URL}/scihub/sentinel2/"
